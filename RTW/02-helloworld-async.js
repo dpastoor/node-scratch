db432708886a4b7b932437b11cb27588d72f0131
Devin Pastoor <devin.pastoor@gmail.com>
2015-05-03 21:14:18 -0400

    add async version and some documentation

--- RTW/02-helloworld-async.js
+++ RTW/02-helloworld-async.js
@@ -1,6 +1,25 @@
 function sayAsync (filename, cb) {
+
 	// async implementation with callback
-	return fs.readFile(filename, cb) 
+	// async read, when get contents back, which could take milliseconds
+	// then check to make sure successful
+	// if not successful immediately error out
+	// else we can simulate doing some other async activity like a database call
+	// so now when we switch back to our command line and run our code
+	// with a real file it will wait before printing (similar to having to wait)
+	// to actually get a file back from the server
+	return fs.readFile(filename, function(err, contents) {
+		// if the error occurs should immediately throw the error up
+		if (err) {
+			cb(err);
+		} else {
+			// setTimout is to simulate something that takes time that we'd handle
+			// in an async fashions
+			setTimeout(function() {
+				cb(null, contents);
+			}, 1000);
+		}
+	}) ;
 }
 
 
