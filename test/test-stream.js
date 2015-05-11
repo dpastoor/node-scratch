ec6e83e2f5bf1baf9069edebd8859b666a7808c1
Devin Pastoor <devin.pastoor@gmail.com>
2015-05-11 11:08:52 -0400

    testing streams

--- test/test-stream.js
+++ test/test-stream.js
@@ -5,9 +5,21 @@ var stream = fs.createReadStream("../concTimeSmall.csv");
 var csvStream = csv
 .parse({headers: true})
 .on("data", function 	 (data) {
+  console.log("first method");
   console.log(data);
 }).on("end", function() { 
   console.log("done");
   });
 
 stream.pipe(csvStream);
+
+//or
+csv
+.fromStream(stream, {headers : true})
+.on("data", function  (data) {
+  console.log('second method');
+  console.log(data);
+})
+.on("end", function  () {
+  console.log("done");
+})
