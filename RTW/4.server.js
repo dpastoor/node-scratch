c2fc26b9ffb7aabbc04f8c5f66e484c82973ce01
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-14 14:58:41 -0800

    add header

--- RTW/4.server.js
+++ RTW/4.server.js
@@ -1,17 +1,23 @@
-/* at this point doing node 4.server.js will start an infinite running process listening on 8006
-* so if go to browser localhost:8006 will see hello world and can use a service like forever to make sure
-* it will also restart
+/*
+* now if we go to the browser and check we will see a network GET request
 */
 
 var http = require("http");
 
 var handleHTTP = function(req, res) {
 	// req is a request STREAM and res is a response STREAM
+
+	//first thing that goes into the header is a status code
+		// success --> 200
+		// additional portions of header in an object
+	res.writeHead(200, {"Content-type": "text/plain"});
+	// to show there can be multiple streams lets also write another line before the end
+	res.write("a first statement : ");
 	res.end("hello world");
 
-}
+};
 
 
 var host = "localhost";
 var port = 8006;
-var http_serv = http.createServer(handleHTTP).listen(port, host)
+var http_serv = http.createServer(handleHTTP).listen(port, host);
