95141efa86e8ba1c1e752fed4aea6d8a31f6be39
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-14 15:11:57 -0800

    implement some example routing logic

--- RTW/4.server.js
+++ RTW/4.server.js
@@ -1,7 +1,6 @@
 /*
-* now if we go to the browser and check we will see a network GET request
+*
 */
-
 var http = require("http");
 
 var handleHTTP = function(req, res) {
@@ -10,10 +9,23 @@ var handleHTTP = function(req, res) {
 	//first thing that goes into the header is a status code
 		// success --> 200
 		// additional portions of header in an object
-	res.writeHead(200, {"Content-type": "text/plain"});
-	// to show there can be multiple streams lets also write another line before the end
-	res.write("a first statement : ");
-	res.end("hello world");
+	if (req.method === "GET") {
+		// likewise theoretically if we only wanted to handle the root route
+		// and only handle if the url is the root
+		if (req.url === "/") {
+			res.writeHead(200, {"Content-type": "text/plain"});
+			// to show there can be multiple streams lets also write another line before the end
+			res.write("a first statement : ");
+			res.end("hello world");
+		} else {
+			res.writeHead(403);
+			res.end("No can do!")
+		}
+	} else {
+		// if we wanted to return a 403 (forbidden) if not a request we want to handle
+		res.writeHead(403);
+		res.end("No can do!")
+	}
 
 };
 
