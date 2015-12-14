0fed678dbbd24c4e78439de22200226f723038b8
Devin Pastoor <devin.pastoor@gmail.com>
2015-12-14 14:41:17 -0800

    add notes

--- RTW/4.server.js
+++ RTW/4.server.js
@@ -1,3 +1,8 @@
+/* at this point doing node 4.server.js will start an infinite running process listening on 8006
+* so if go to browser localhost:8006 will see hello world and can use a service like forever to make sure
+* it will also restart
+*/
+
 var http = require("http");
 
 var handleHTTP = function(req, res) {
